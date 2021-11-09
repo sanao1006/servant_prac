cd77ea623ae0ffcc149a5c1e75b537cc41fdc5ca
sanao1006 <naosak1006@gmail.com>
2021-11-09 21:55:08 +0900

    section4

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,57 +2,90 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-
 module Lib
     ( runServant
     ) where
 
 import Servant
-import Data.Aeson(ToJSON)
+import Data.Aeson(ToJSON, FromJSON)
 import Data.Time.Calendar
+import Data.List
+import Data.Maybe
 import GHC.Generics(Generic)
 import Network.Wai(Application)
 import Network.Wai.Handler.Warp(run)
 
-data User = User
- {
-     name :: String,
-     age :: Int,
-     email :: String,
-     registration_date :: Day
- } deriving(Eq,Show, Generic)
+type API = "position" :> Capture "x" Int :> Capture "y" Int :> Get '[JSON] Position
+      :<|> "hello" :> QueryParam "name" String :> Get '[JSON] HelloMessage
+      :<|> "marketing" :> ReqBody '[JSON] ClientInfo :> Post '[JSON] Email
 
-instance ToJSON User
+data Position = Position
+  { xCoord :: Int
+  , yCoord :: Int
+  } deriving Generic
 
-isaac :: User
-isaac = User "Isaac Newton" 372 "isac@newton.co.uk"(fromGregorian 1683 3 1)
-albert :: User
-albert = User "Albert Einstein" 136 "ae@mc2.org"(fromGregorian 1905 12 1)
+instance ToJSON Position
 
-sanao :: User
-sanao = User "Sanao" 17 "sanasana@sana" (fromGregorian 2017 1 1)
+newtype HelloMessage = HelloMessage { msg :: String }
+  deriving Generic
 
-users2 :: [User]
-users2 = [isaac, albert,sanao]
+instance ToJSON HelloMessage
 
+data ClientInfo = ClientInfo
+  { clientName :: String
+  , clientEmail :: String
+  , clientAge :: Int
+  , clientInterestedIn :: [String]
+  } deriving Generic
 
-type UserAPI2 = "users" :> Get '[JSON] [User]
-           :<|> "albert" :> Get '[JSON] User
-           :<|> "isaac" :> Get '[JSON] User
-           :<|> "sanao" :> Get '[JSON] User
+instance FromJSON ClientInfo
+instance ToJSON ClientInfo
 
-server2 :: Server UserAPI2
-server2 = return users2
-     :<|> return albert
-     :<|> return isaac
-     :<|> return sanao
+data Email = Email
+  { from :: String
+  , to :: String
+  , subject :: String
+  , body :: String
+  } deriving Generic
 
-userAPI :: Proxy UserAPI2
+instance ToJSON Email
+
+emailForClient :: ClientInfo -> Email
+emailForClient c = Email from' to' subject' body'
+
+  where from'    = "great@company.com"
+        to'      = clientEmail c
+        subject' = "Hey " ++ clientName c ++ ", we miss you!"
+        body'    = "Hi " ++ clientName c ++ ",\n\n"
+                ++ "Since you've recently turned " ++ show (clientAge c)
+                ++ ", have you checked out our latest "
+                ++ intercalate ", " (clientInterestedIn c)
+                ++ " products? Give us a visit!"
+
+server3 :: Server API
+server3 = position
+     :<|> hello
+     :<|> marketing
+
+  where position :: Int -> Int -> Handler Position
+        position x y = return (Position x y)
+
+        hello :: Maybe String -> Handler HelloMessage
+        hello mname = return . HelloMessage $ case mname of
+          Nothing -> "Hello, anonymous coward"
+          Just n  -> "Hello, " ++ n
+
+        marketing :: ClientInfo -> Handler Email
+        marketing clientinfo = return (emailForClient clientinfo)
+
+userAPI :: Proxy API
 userAPI = Proxy
 
-app2 :: Application
-app2 = serve userAPI server2
+-- 'serve' comes from servant and hands you a WAI Application,
+-- which you can think of as an "abstract" web application,
+-- not yet a webserver.
+app3 :: Application
+app3 = serve userAPI server3
 
 runServant :: IO ()
-runServant = run 8081 app2
-
+runServant = run 8081 app3
